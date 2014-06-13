281fc0407f762154b93088af8eb5e7dacc958e26
Trylobot <trylobot@gmail.com>
2014-06-13 14:02:19 -0500

    Create hive-ai-max.js

---
+++ ai/max/hive-ai-max.js
@@ -0,0 +1,9 @@
+// require hive/core/*
+
+/*
+hive-ai-max.js
+  "Max"
+  This AI module utilizes standard minimax search and alpha-beta pruning, along with a board scoring heuristic, to decide on a move.
+  Since it is writtin in Javascript, it is also able to leverage the same core game modules as the game engine itself.
+
+*/
