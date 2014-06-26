a88a5dc423540818ad5f63968e61dab18f17002d
Trylobot <trylobot@gmail.com>
2014-06-25 19:05:28 -0500

    fixed bower dependency specificiation for tween.js by using the official 'bower name' for the repo and specifying a repo tag as the version

--- nw/release_package_linux.bat
+++ nw/release_package_linux.bat
@@ -1,2 +1,2 @@
 rm dist/win/app.nw
-7z a -tzip dist/win/app.nw  *.json *.png *.css *.js *.html fonts/*.otf  ../core/  node_modules/mersenne/  node_modules/async/  node_modules/lodash/  bower_components/tween.js/src/Tween.js  bower_components/pixi.js/bin/pixi.dev.js
+7z a -tzip dist/win/app.nw  *.json *.png *.css *.js *.html fonts/*.otf  ../core/  node_modules/mersenne/  node_modules/async/  node_modules/lodash/  bower_components/tweenjs/src/Tween.js  bower_components/pixi.js/bin/pixi.dev.js
